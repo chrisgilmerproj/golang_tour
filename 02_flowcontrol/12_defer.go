ebcd7969da14eb29f9c9d03c308c98784a647a67
Chris Gilmer <chris.gilmer@gmail.com>
2014-12-28 09:39:13 -0500

    Add flowcontrol lessons

---
+++ 02_flowcontrol/12_defer.go
@@ -0,0 +1,19 @@
+/*
+Defer
+A defer statement defers the execution of a function until the surrounding function returns.
+
+The deferred call's arguments are evaluated immediately, but the function call is not executed until the surrounding function returns.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+    defer fmt.Println("world 1")
+    defer fmt.Println("world 2")
+    defer fmt.Println("world 3")
+
+    fmt.Println("hello")
+}
+
